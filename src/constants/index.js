1bf04e2919e378d5708246f5a20c7f829e42d992
Yash290402 <yash56774@gmail.com>
2024-10-16 14:54:27 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,271 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    helpershands,
+    Verbalvibes,
+    imagenie,
+    hungrito,
+    java,
+    c,
+    GSAP
+} from "../assets/assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+const services = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "React Developer",
+        icon: mobile,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    },
+    {
+        title: "knowledge about Database like MongoDB,MySQL...",
+        icon: creator,
+    },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "java",
+        icon: java,
+    }, 
+    {
+        name: "c",
+        icon: c,
+    },
+    {
+        name: "GSAP",
+        icon: GSAP,
+    }
+
+];
+
+const experiences = [
+    {
+        "title": "Helpers Hands",
+        "technologies": "JavaScript, React, Node, Express, Tailwind, MongoDB",
+        "icon": helpershands,
+        "iconBg": "#383E56",
+        "date": "April 2024 - June 2024",
+        repoLink: "https://github.com/Yash290402/HelpersHand",
+        "points": [
+            "Developed a responsive website used to book any required, skilled and trained, service provider by the user, at its respective selected booking slot.",
+            "Features: Searching for Service providers, Booking, Selecting slots, Feedback, etc.",
+           
+        ],
+       
+    },
+    {
+        title: "VerbalVibe",
+        technologies: "React, Tailwind, firebase as a Backend service",
+        icon: Verbalvibes,
+        iconBg: "#E6DEDD",
+        date: "Jan 2024 - March 2024",
+        repoLink: "https://github.com/Yash290402/VerbalVibe",
+        points: [
+            "VerbalVibe is a messaging app offering smooth communication with a user-friendly interface, real-time messaging, multimedia sharing, and robust security across all platforms.",
+            "Features: User-friendly interface, real-time messaging, multimedia sharing, and robust security measures",
+            
+        ],
+    },
+    {
+        title: "ImaGenie",
+        technologies: "React, Tailwind, Javascript, Node.js, Express.js, MongoDB",
+        icon: imagenie,
+        iconBg: "#383E56",
+        date: "Nov 2023 - December 2023",
+        repoLink: "https://github.com/Yash290402/ImaGenie",
+        points: [
+            "ImaGenie is a website that requests a prompt for an image and generates the requested image using OpenAI's API. It also features a functionality where users can share the generated images.",
+            "Features: Generating, downloading and sharing the image.",
+          
+        ],
+    },
+    {
+        title: "Hungrito",
+        technologies: "HTML,CSS,JavaScript,Node.js,Express.js,MongoDB",
+        icon: hungrito,
+        iconBg: "#E6DEDD",
+        date: "March 2023 - July 2023",
+       
+        points: [
+            "Hungrito is an innovative platform designed to revolutionize the dining experience by offering seamless online ordering and table booking services.",
+            " Built with a focus on user convenience, Hungrito allows customers to browse restaurant menus, place orders for delivery or pickup, and reserve tables at their favorite dining spots—all from a single, user-friendly interface.",
+            "The platform is designed to cater to both customers and restaurant owners, providing a streamlined solution that enhances operational efficiency while delivering a superior customer experience.",
+
+        ],
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        name: "Sara Lee",
+        designation: "CFO",
+        company: "Acme Co",
+        image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+        testimonial:
+            "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        name: "Chris Brown",
+        designation: "COO",
+        company: "DEF Corp",
+        image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+        testimonial:
+            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        name: "Lisa Wang",
+        designation: "CTO",
+        company: "456 Enterprises",
+        image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+];
+
+const projects = [
+    {
+        name: "Car Rent",
+        description:
+            "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: carrent,
+        source_code_link: "https://github.com/",
+    },
+    {
+        name: "Job IT",
+        description:
+            "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: jobit,
+        source_code_link: "https://github.com/",
+    },
+    {
+        name: "Trip Guide",
+        description:
+            "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        tags: [
+            {
+                name: "nextjs",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "supabase",
+                color: "green-text-gradient",
+            },
+            {
+                name: "css",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: tripguide,
+        source_code_link: "https://github.com/",
+    },
+];
+
+export { services, technologies, experiences, testimonials, projects };
